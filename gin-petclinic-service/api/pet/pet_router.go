480f595cd9fb13db135052700bb6dc6a3e913561
Ryan Tran <rhardytran@gmail.com>
2025-08-08 16:17:28 -0700

    implement fiber3 air

--- gin-petclinic-service/api/pet/pet_router.go
+++ gin-petclinic-service/api/pet/pet_router.go
@@ -1,5 +1,17 @@
 package pet
 
+import (
+	"errors"
+	"gin-petclinic-service/api"
+	resterr "gin-petclinic-service/pkg/errors"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"gorm.io/gorm"
+)
+
 type Router struct {
 	service Servicer
 }
@@ -10,220 +22,238 @@ func NewRouter(service Servicer) *Router {
 }
 
 // Register - register routes
-//func (router *Router) Register(routerGroup *gin.RouterGroup) {
-//	routerGroup.GET("all", router.getAll)
-//	routerGroup.GET(":id", router.getById)
-//	routerGroup.GET(":id/visits", router.getWithVisitsById)
-//	routerGroup.GET("", router.getByQueryParam)
-//	routerGroup.POST("", router.create)
-//	routerGroup.PUT(":id", router.update)
-//}
+func (router *Router) Register(routerGroup *gin.RouterGroup) {
+	routerGroup.GET("all", router.getAll)
+	routerGroup.GET(":id", router.getById)
+	routerGroup.GET(":id/visits", router.getWithVisitsById)
+	routerGroup.GET("", router.getByQueryParam)
+	routerGroup.POST("", router.create)
+	routerGroup.PUT(":id", router.update)
+}
+
+// allPets - retrieve all pets
+// @Tags		pets
+// @Summary	List all pets
 //
-//// allPets - retrieve all pets
-//// @Tags		pets
-//// @Summary	List all pets
-////
-//// @Description	Get all pets
-//// @Produce		json
-//// @Success		200	{object}	Responses
-//// @Failure		404	{object}	errors.ErrorResponse
-//// @Failure		500	{object}	errors.ErrorResponse
-//// @Router		/pets/all 		[get]
-//func (router *Router) getAll(c *gin.Context) {
-//	log.Info().Msg("Retrieve all pets")
-//	responses, err := router.service.getAllPets()
-//	if err != nil {
-//		log.Error().Err(err).Msg("Unable to get all pets.")
-//		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(err.Error()))
-//		return
-//	}
+// @Description	Get all pets
+// @Produce		json
+// @Success		200	{object}	Responses
+// @Failure		404	{object}	errors.ErrorResponse
+// @Failure		500	{object}	errors.ErrorResponse
+// @Router		/pets/all 		[get]
+func (router *Router) getAll(c *gin.Context) {
+	log.Info().Msg("Retrieve all pets")
+	responses, err := router.service.getAllPets()
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Error().Err(err).Msg("find no pets.")
+			c.JSON(http.StatusNotFound, resterr.NotFound("No pets found."))
+			return
+		}
+		log.Error().Err(err).Msg("Unable to get all pets.")
+		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusOK, responses)
+}
+
+// petById - retrieve pet by id
+// @Tags		pets
+// @Summary		Get pet by ID
 //
-//	c.JSON(http.StatusOK, responses)
-//}
+// @Description	Get pet by ID
+// @Param		id	path	int	true	"Pet ID"
+// @Produce		json
+// @Success		200	{object}	Responses
+// @Failure		404	{object}	errors.ErrorResponse
+// @Failure		500	{object}	errors.ErrorResponse
+// @Router		/pets/{id} 		[get]
+func (router *Router) getById(c *gin.Context) {
+	log.Info().Msg("Retrieve pet by id")
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to convert to number.")
+		c.JSON(http.StatusBadRequest, resterr.BadRequest(err.Error()))
+		return
+	}
+
+	response, err := router.service.getPetById(uint(id))
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Error().Err(err).Int("id", id).Msg("find no pet by this id.")
+			c.JSON(http.StatusNotFound, resterr.NotFound(err.Error()))
+			return
+		}
+		log.Error().Err(err).Int("id", id).Msg("Unable to get pet by id.")
+		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusOK, response)
+}
+
+// petWithVisitsById - get pet with visits by id
+// @Tags		pets
+// @Summary		Get pet with visits by ID
 //
-//// petById - retrieve pet by id
-//// @Tags		pets
-//// @Summary		Get pet by ID
-////
-//// @Description	Get pet by ID
-//// @Param		id	path	int	true	"Pet ID"
-//// @Produce		json
-//// @Success		200	{object}	Responses
-//// @Failure		404	{object}	errors.ErrorResponse
-//// @Failure		500	{object}	errors.ErrorResponse
-//// @Router		/pets/{id} 		[get]
-//func (router *Router) getById(c *gin.Context) {
-//	log.Info().Msg("Retrieve pet by id")
-//	id, err := strconv.Atoi(c.Param("id"))
-//	if err != nil {
-//		log.Error().Err(err).Msg("Unable to convert to number.")
-//		c.JSON(http.StatusBadRequest, resterr.BadRequest(err.Error()))
-//		return
-//	}
+// @Description	Get pet with visits by ID
+// @Param		id	path	int	true	"Pet ID"
+// @Produce		json
+// @Success		200	{object}	Responses
+// @Failure		400	{object}	errors.ErrorResponse
+// @Failure		404	{object}	errors.ErrorResponse
+// @Failure		500	{object}	errors.ErrorResponse
+// @Router		/pets/{id}/visits 		[get]
+func (router *Router) getWithVisitsById(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to convert to number.")
+		c.JSON(http.StatusBadRequest, resterr.BadRequest(err.Error()))
+		return
+	}
+
+	response, err := router.service.getPetWithVisitsById(uint(id))
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Error().Err(err).Msg("find no pet by this id.")
+			c.JSON(http.StatusNotFound, resterr.NotFound(err.Error()))
+			return
+		}
+		log.Error().Err(err).Msg("Unable to get pet by id.")
+		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusOK, response)
+}
+
+// petByQueryParam -
+// @Tags		pets
+// @Summary		Get pet by name
 //
-//	response, err := router.service.getPetById(uint(id))
-//	if err != nil {
-//		if errors.Is(err, gorm.ErrRecordNotFound) {
-//			log.Error().Err(err).Int("id", id).Msg("find no pet by this id.")
-//			c.JSON(http.StatusNotFound, resterr.NotFound(err.Error()))
-//			return
-//		}
-//		log.Error().Err(err).Int("id", id).Msg("Unable to get pet by id.")
-//		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(err.Error()))
-//		return
-//	}
+// @Description	Get pet by name
+// @Param		name	query	string	true	"Pet Name"
+// @Produce		json
+// @Success		200	{object}	Responses
+// @Failure		404	{object}	errors.ErrorResponse
+// @Failure		404	{object}	errors.ErrorResponse
+// @Failure		500	{object}	errors.ErrorResponse
+// @Router		/pets 			[get]
+func (router *Router) getByQueryParam(c *gin.Context) {
+	var nameParam api.NameParam
+	err := c.BindQuery(&nameParam)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to bind query param.")
+		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
+		return
+	}
+
+	router.getByName(c, nameParam)
+}
+
+// petByName - get pet by name
+func (router *Router) getByName(c *gin.Context, param api.NameParam) {
+	pets, err := router.service.getPetsByName(param.Name)
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Error().Err(err).Str("name", param.Name).Msg("find no pet by this name")
+			c.JSON(http.StatusNotFound, resterr.NotFound("No pets found with name: "+param.Name))
+			return
+		}
+		log.Error().Err(err).Str("name", param.Name).Msg("Unable to get pets by name.")
+		// If there is an error other than not found, return internal server
+		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(""))
+		return
+	}
+
+	c.JSON(http.StatusOK, pets)
+}
+
+// addNewPet - add new pet
+// @Tags		pets
+// @Summary		Add a new pet
 //
-//	c.JSON(http.StatusOK, response)
-//}
+// @Description	Add a new pet
+// @Param		Request	body	AddRequest	true	"Add pet"
+// @Produce		json
+// @Success		200	{object}	Responses
+// @Failure		400	{object}	errors.ErrorResponse
+// @Failure		404	{object}	errors.ErrorResponse
+// @Failure		500	{object}	errors.ErrorResponse
+// @Router		/pets	 		[post]
+func (router *Router) create(c *gin.Context) {
+	var request AddRequest
+	err := c.ShouldBind(&request)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to Unmarshal JSON.")
+		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
+		return
+	}
+
+	log.Info().Str("name", request.Name).Msg("Add a new pet.")
+	petEntity, err := FromAddRequest(&request)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to convert request to pet entity.")
+		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
+		return
+	}
+
+	petResponse, err := router.service.create(petEntity)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to create pet.")
+		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(err.Error()))
+		return
+	}
+	c.JSON(http.StatusCreated, petResponse)
+}
+
+// updatePet - update pet
+// @Tags		pets
+// @Summary		update a pet
 //
-//// petWithVisitsById - get pet with visits by id
-//// @Tags		pets
-//// @Summary		Get pet with visits by ID
-////
-//// @Description	Get pet with visits by ID
-//// @Param		id	path	int	true	"Pet ID"
-//// @Produce		json
-//// @Success		200	{object}	Responses
-//// @Failure		400	{object}	errors.ErrorResponse
-//// @Failure		404	{object}	errors.ErrorResponse
-//// @Failure		500	{object}	errors.ErrorResponse
-//// @Router		/pets/{id}/visits 		[get]
-//func (router *Router) getWithVisitsById(c *gin.Context) {
-//	id, err := strconv.Atoi(c.Param("id"))
-//	if err != nil {
-//		log.Error().Err(err).Msg("Unable to convert to number.")
-//		c.JSON(http.StatusBadRequest, resterr.BadRequest(err.Error()))
-//		return
-//	}
-//
-//	response, err := router.service.getPetWithVisitsById(uint(id))
-//	if err != nil {
-//		if errors.Is(err, gorm.ErrRecordNotFound) {
-//			log.Error().Err(err).Msg("find no pet by this id.")
-//			c.JSON(http.StatusNotFound, resterr.NotFound(err.Error()))
-//			return
-//		}
-//		log.Error().Err(err).Msg("Unable to get pet by id.")
-//		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(err.Error()))
-//		return
-//	}
-//
-//	c.JSON(http.StatusOK, response)
-//}
-//
-//// petByQueryParam -
-//// @Tags		pets
-//// @Summary		Get pet by name
-////
-//// @Description	Get pet by name
-//// @Param		name	query	string	true	"Pet Name"
-//// @Produce		json
-//// @Success		200	{object}	Responses
-//// @Failure		404	{object}	errors.ErrorResponse
-//// @Failure		404	{object}	errors.ErrorResponse
-//// @Failure		500	{object}	errors.ErrorResponse
-//// @Router		/pets 			[get]
-//func (router *Router) getByQueryParam(c *gin.Context) {
-//	var nameParam api.NameParam
-//	err := c.BindQuery(&nameParam)
-//	if err != nil {
-//		log.Error().Err(err).Msg("Unable to bind query param.")
-//		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
-//		return
-//	}
-//
-//	router.getByName(c, nameParam)
-//}
-//
-//// petByName - get pet by name
-//func (router *Router) getByName(c *gin.Context, param api.NameParam) {
-//	pets, err := router.service.getPetsByName(param.Name)
-//
-//	if err != nil {
-//		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(""))
-//		return
-//	}
-//
-//	if len(pets) == 0 {
-//		log.Error().Err(err).Str("name", param.Name).Msg("find no pet by this name")
-//		c.JSON(http.StatusNotFound, resterr.NotFound("No pets found with name: "+param.Name))
-//		return
-//	}
-//
-//	responses := Responses{
-//		Context: model.Context{
-//			Count: len(pets),
-//		},
-//		Pets: pets,
-//	}
-//	c.JSON(http.StatusOK, responses)
-//}
-//
-//// addNewPet - add new pet
-//// @Tags		pets
-//// @Summary		Add a new pet
-////
-//// @Description	Add a new pet
-//// @Param		Request	body	AddRequest	true	"Add pet"
-//// @Produce		json
-//// @Success		200	{object}	Responses
-//// @Failure		400	{object}	errors.ErrorResponse
-//// @Failure		404	{object}	errors.ErrorResponse
-//// @Failure		500	{object}	errors.ErrorResponse
-//// @Router		/pets	 		[post]
-//func (router *Router) create(c *gin.Context) {
-//	var request Request
-//	err := c.ShouldBind(&request)
-//	if err != nil {
-//		log.Error().Err(err).Msg("Unable to Unmarshal JSON.")
-//		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
-//		return
-//	}
-//
-//	log.Info().Str("name", request.Name).Msg("Add a new pet.")
-//	petResponse, err := router.service.create(ToPet(&request))
-//	c.JSON(http.StatusCreated, petResponse)
-//}
-//
-//// updatePet - update pet
-//// @Tags		pets
-//// @Summary		update a pet
-////
-//// @Description	update pet
-//// @Param		id	path	int	true	"Pet ID"
-//// @Param		Request	body	UpdateRequest	true	"Update pet"
-//// @Produce		json
-//// @Success		200	{object}	Responses
-//// @Failure		400	{object}	errors.ErrorResponse
-//// @Failure		404	{object}	errors.ErrorResponse
-//// @Failure		500	{object}	errors.ErrorResponse
-//// @Router		/pets/{id}	 	[put]
-//func (router *Router) update(c *gin.Context) {
-//	var request Request
-//	id, err := strconv.Atoi(c.Param("id"))
-//	if err != nil {
-//		log.Error().Err(err).Msg("Unable to convert to number.")
-//		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
-//		return
-//	}
-//
-//	err = c.ShouldBind(&request)
-//	if err != nil {
-//		log.Error().Err(err).Msg("Unable to Unmarshal JSON.")
-//		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
-//		return
-//	}
-//
-//	log.Info().Str("name", request.Name).Msg("Update a pet.")
-//	petEntity := ToPet(&request)
-//	petEntity.ID = uint(id)
-//	petResponse, err := router.service.update(petEntity)
-//
-//	if err != nil {
-//		log.Error().Err(err).Msg("Unable to update pet.")
-//		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(err.Error()))
-//		return
-//	}
-//
-//	c.JSON(http.StatusCreated, petResponse)
-//}
+// @Description	update pet
+// @Param		id	path	int	true	"Pet ID"
+// @Param		Request	body	UpdateRequest	true	"Update pet"
+// @Produce		json
+// @Success		200	{object}	Responses
+// @Failure		400	{object}	errors.ErrorResponse
+// @Failure		404	{object}	errors.ErrorResponse
+// @Failure		500	{object}	errors.ErrorResponse
+// @Router		/pets/{id}	 	[put]
+func (router *Router) update(c *gin.Context) {
+	var request UpdateRequest
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to convert to number.")
+		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
+		return
+	}
+
+	err = c.ShouldBind(&request)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to Unmarshal JSON.")
+		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
+		return
+	}
+
+	log.Info().Str("name", request.Name).Msg("Update a pet.")
+	petEntity, err := FromUpdateRequest(&request)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to convert request to pet entity.")
+		c.JSON(http.StatusBadRequest, resterr.BadRequestWithDetails(err))
+		return
+	}
+
+	petEntity.ID = uint(id)
+	petResponse, err := router.service.update(petEntity)
+
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to update pet.")
+		c.JSON(http.StatusInternalServerError, resterr.InternalServerError(err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusCreated, petResponse)
+}
