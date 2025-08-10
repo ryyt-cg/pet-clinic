00c15543de868be7fe984573f7f53ac304f02468
Ryan Tran <rhardytran@gmail.com>
2025-08-10 14:41:05 -0700

    feat: add pet-clinic web app

---
+++ pet-clinic-web/src/app.tsx
@@ -0,0 +1,26 @@
+import {createRouter, RouterProvider} from '@tanstack/react-router'
+
+// Import the generated route tree
+import {routeTree} from '@/routeTree.gen'
+import {ThemeProvider} from './context/theme-context.tsx'
+
+// Create a new router instance
+const router = createRouter({routeTree})
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+
+function App() {
+  return (
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router}/>
+      </ThemeProvider>
+  )
+}
+
+export default App
---
+++ pet-clinic-web/src/features/home/index.tsx
@@ -0,0 +1,12 @@
+import {ThemeSwitch} from "@/components/theme-switch.tsx";
+
+const Home = () => {
+  return (
+      <div>
+        <ThemeSwitch/>
+        Home Page
+      </div>
+  );
+};
+
+export default Home;
