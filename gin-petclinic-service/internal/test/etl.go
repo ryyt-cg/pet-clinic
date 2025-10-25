b04be7129088665a54d58819911c5aa4df834e8a
Ryan Tran <rhardytran@gmail.com>
2025-10-25 08:20:35 -0700

    refactor: remove mock service

--- gin-petclinic-service/internal/test/etl.go
+++ gin-petclinic-service/internal/test/etl.go
@@ -1,16 +1,21 @@
 package test
 
-import jsoniter "github.com/json-iterator/go"
+import (
+	"time"
+
+	jsoniter "github.com/json-iterator/go"
+)
 
 func JsonString(obj interface{}) string {
 	b, _ := jsoniter.Marshal(obj)
 	return string(b)
 }
 
-//func ToDate(dateStr string) *time.Time {
-//	date, err := time.Parse(time.DateOnly, dateStr)
-//	if err != nil {
-//		return nil
-//	}
-//	return &date
-//}
+// ToDate - convert date string to date
+func ToDate(dateStr string) *time.Time {
+	date, err := time.Parse(time.DateOnly, dateStr)
+	if err != nil {
+		return nil
+	}
+	return &date
+}
